3144a0a6b16b77a72a7eb32f0621eaafc3bca7be
Dennis <dennis.jutemark@telia.com>
2013-05-23 13:53:53 +0200

    Added more gui

--- src/TheGame/Gui.java
+++ src/TheGame/Gui.java
@@ -2,12 +2,31 @@ package TheGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Gui extends JFrame {
+	String tfS;
+	String pfS = "myPass";
 	
 	public Gui() {
-		JTextField tf = new JTextField();
-		tf.setBackground(Color.YELLOW);
-		add(tf, BorderLayout.SOUTH);
+		JTextField tf = new JTextField("Enter Username", 10);
+		JPasswordField pf = new JPasswordField(pfS, 10);
+		setLayout(new FlowLayout());
+		add(tf);
+		add(pf);
+		
+		TheHandler handler = new TheHandler();
+		tf.addActionListener(handler);
+		pf.addActionListener(handler);
 	}
+	
+	private class TheHandler implements ActionListener {
+		
+		public void actionPerformed(ActionEvent event) {
+			
+		}
+		
+	}
+	
 }
