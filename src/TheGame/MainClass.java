e34a2024bd954823016895f6a7b134a0e49ba3ca
Dennis <dennis.jutemark@telia.com>
2013-05-23 11:07:58 +0200

    Nästa test

--- src/TheGame/MainClass.java
+++ src/TheGame/MainClass.java
@@ -1,10 +1,19 @@
 package TheGame;
 
+import java.util.Scanner;
+
 public class MainClass {
 
 	public void main(String args[]){
+		int fnum, snum;
+		Scanner sc = new Scanner(System.in);
 		String y;
+		
 		y = "This is a test string";
 		System.out.println(y);
+		
+		System.out.println("Testrad");
+		
+		Testsync to = new Testsync();
 	}
 }
